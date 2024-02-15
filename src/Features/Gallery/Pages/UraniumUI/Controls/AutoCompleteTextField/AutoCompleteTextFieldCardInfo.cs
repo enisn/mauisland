70e194693398facfd71ab6edff1ad3679bd02f8a
Enis Necipoglu <info@enisnecipoglu.com>
2024-02-15 23:49:52 +0300

    Add AutoCompleteTextFieldPage

--- src/Features/Gallery/Pages/UraniumUI/Controls/AutoCompleteTextField/AutoCompleteTextFieldCardInfo.cs
+++ src/Features/Gallery/Pages/UraniumUI/Controls/AutoCompleteTextField/AutoCompleteTextFieldCardInfo.cs
@@ -1,9 +1,13 @@
-﻿using UraniumUI.Icons.SegoeFluent;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UraniumUI.Icons.SegoeFluent;
 using UraniumUI.Material.Controls;
 
 namespace MAUIsland;
-
-public class TextFieldCardInfo : IGalleryCardInfo
+public class AutoCompleteTextFieldCardInfo : IGalleryCardInfo
 {
     public ImageSource ControlIcon { get; } = new FontImageSource
     {
@@ -11,11 +15,11 @@ public class TextFieldCardInfo : IGalleryCardInfo
         Glyph = Fluent.Rename,
         Size = 100
     };
-    public string ControlName { get; } = nameof(TextField);
+    public string ControlName { get; } = nameof(AutoCompleteTextField);
     public string ControlDetail { get; } = "The .NET MAUI TextField control provides an input control that accepts text input from a user.";
-    public string ControlRoute { get; } = typeof(TextFieldPage).FullName;
+    public string ControlRoute { get; } = typeof(AutoCompleteTextFieldPage).FullName;
     public string GitHubUrl { get; } = "https://github.com/Strypper/mauisland/tree/main/src/Features/Gallery/Pages/UraniumUI/";
-    public string DocumentUrl { get; } = "https://enisn-projects.io/docs/en/uranium/latest/themes/material/components/TextField";
+    public string DocumentUrl { get; } = "https://enisn-projects.io/docs/en/uranium/latest/themes/material/components/AutoCompleteTextField";
     public string GroupName => ControlGroupInfo.UraniumUIComponent;
     public GalleryCardType CardType { get; } = GalleryCardType.Control;
     public GalleryCardStatus CardStatus { get; } = GalleryCardStatus.Completed;
---
+++ src/Features/Gallery/Pages/UraniumUI/Controls/AutoCompleteTextField/AutoCompleteTextFieldPage.xaml.cs
@@ -0,0 +1,9 @@
+namespace MAUIsland;
+
+public partial class AutoCompleteTextFieldPage : IGalleryPage
+{
+	public AutoCompleteTextFieldPage()
+	{
+		InitializeComponent();
+	}
+}
